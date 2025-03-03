cbeab140d02dec9c224dedd9fa3d7e5961a26d83
adrien-ge <adrien.geraldes@gmail.com>
2025-03-02 22:24:16 -0500

    ok

--- js/importWords.js
+++ js/importWords.js
@@ -1,5 +1,5 @@
 import { db } from "../js/firebase-config.js";
-import { collection, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
 // 📌 Fonction pour supprimer tous les mots avant importation
